296dcf81a7398c841cddb7597ec1b38ac154729b
Hasmik <[hasmik.alikhanyan1@gmail.com]>
2023-05-23 23:56:19 +0400

    .

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   "connection": {
-    "url": 'mongodb+srv://hasmikalikhanyan1:eqOlLCyDlhtYxim7@todo.3fhkvrx.mongodb.net/?retryWrites=true&w=majority'
-
-    // "url": process.env.MONGO_API
-
+    "url": process.env.MONGO_API
   },
 };
